3a63585561186d5273b6a71a7bac6c53e1b92f7b
hypolas <nicolas.hypolite@gmail.com>
2023-03-05 02:03:00 +0100

    Init push

---
+++ init_test.go
@@ -0,0 +1,44 @@
+package hypolashlckhelpers
+
+import (
+	"bufio"
+	"fmt"
+	logg "github.com/hypolas/hypolaslogger"
+	"os"
+	"testing"
+)
+
+// Test if log is wheel write.
+func TestLogs(t *testing.T) {
+	os.Setenv("HYPOLAS_LOGS_FILE", "test/test.log")
+	os.Setenv("HYPOLAS_HEALTHCHECK_HTTP_URL", "http://#CMDSTART# hostname #CMDEND#:8082/ping")
+
+	fpath := os.Getenv("HYPOLAS_LOGS_FILE")
+	// Remove existing test file
+	os.Remove(fpath)
+
+	log := logg.NewLogger("")
+
+	vars := InitHlckCustom{}
+	v := vars.GenerateVars("HYPOLAS_HEALTHCHECK_HTTP_URL", "titi")
+	log.Info.Println(v)
+
+	// Test variable
+	var tesvar os.Process
+	log.VarDebug(tesvar, "tesvar")
+
+	readFile, err := os.Open(fpath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+	fileScanner := bufio.NewScanner(readFile)
+
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fmt.Println(fileScanner.Text())
+	}
+
+	readFile.Close()
+}
